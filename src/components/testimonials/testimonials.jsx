726cfea6cba14d1c007707bbfaa0daddc4cf64d2
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-29 11:29:12 -0500

    added testimonials component folder and files

---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//connect individual style sheet
+import './testimonials.css'
+
+const testimonials = () => {
+  return (
+    <div>testimonials</div>
+  )
+}
+
+export default testimonials
