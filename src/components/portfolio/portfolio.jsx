f22bf125451e1d4a507ff16ce2fd383969c370e0
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-30 12:30:19 -0500

    laid out template for portfolio.jsx

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
 //connect individual style sheet
 import './portfolio.css'
+//portfolio images
+import IMG1 from '../../assets/mixituplogodark.png'
+// import IMG2 from '../../assets/'
+// import IMG3 from '../../assets/'
+// import IMG4 from '../../assets/'
+// import IMG5 from '../../assets/'
+// import IMG6 from '../../assets/'
+
 
 const portfolio = () => {
   return (
-    <div>portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="comtainer portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Neon logo with two martini glasses" />
+          </div>
+            <h3>Mix It Up: Cocktail Recipe Database</h3>
+            <a href="https://github.com/sergeyholin/Mix-It-Up" className='btn' target='blank'>Github</a>
+            <a href="https://adadlaldalkmdlka.herokuapp.com/" className='btn btn-primary' target='blank'>Live Demo</a>
+        </article>
+
+
+      </div>
+      </section>
   )
 }
 
