f9fc63fad839e7e08f59ce515a17882eb2615706
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-30 02:54:48 -0500

    added p tags and began styling aboutme section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -16,7 +16,7 @@ const about = () => {
 
      <div className = 'container about_container'>
        <div className='about_me'>
-          <div className="about_me-image">
+          <div className='about_me-image'>
             <img src={ME} alt='photo of Jeffrey' />
           </div>
        </div>
@@ -38,6 +38,15 @@ const about = () => {
             <small>The Priory of the Orange Tree</small>
           </article>
         </div>
+
+        <p>
+           I'm Jeffrey, a full-stack developer with a passion for delievering <span>⭐⭐⭐⭐⭐</span> designs that are elegant and intuitive.  While the majority of my experience has been developing brand and e-commerce sites, I have also worked on a broad spectrum project types.  I'm no slouch when it comes to backend development either, as I've created multiple full-stack projects and have experience with business intelligence data management. 
+        </p>
+        <p>
+          As a developer, I'm most driven by taking a clients' idea and making it a reality.  I don't adhere to any one style, because YOUR website should be a true representation of YOU or your brand's style and vibe.  I hope you will share your vision and allow me to help shape your digital world.
+        </p>
+        
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
        </div>
      </div>
     </section>
