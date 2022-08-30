ab1f7f2570f690255bf125bca6a3ec5d6e430e50
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-30 01:15:41 -0500

    navbar component completed

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -7,17 +7,21 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {RiSuitcaseLine} from 'react-icons/ri'
 import {FaLaptopCode} from 'react-icons/fa'
 import {FiMessageSquare} from 'react-icons/fi'
+// import the useState hook for dynamic active selection on navbar buttons
+import { useState } from 'react';
 
-const nav = () => {
+const Nav = () => {
+  // variable for active button on navbar
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><AiOutlineHome /></a>
-      <a href='#about'><AiOutlineUser /></a>
-      <a href='#experience'><RiSuitcaseLine /></a>
-      <a href='#services'><FaLaptopCode /></a>
-      <a href='#contact'><FiMessageSquare /></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><RiSuitcaseLine /></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FaLaptopCode /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><FiMessageSquare /></a>
     </nav>
   )
 }
 
-export default nav
+export default Nav
