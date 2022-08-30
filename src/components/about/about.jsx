e89479beed9cc700d5f2979c06d3b5753c800aa6
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-30 04:05:31 -0500

    about me finished, icebox family info

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import './about.css'
 //import image for aboutme
 import ME from '../../assets/upwardlook.jpg'
+import US from '../../assets/us.JPG'
 //import icons for aboutme
 import {RiHeartsFill} from 'react-icons/ri'
 import {FaDiceD20} from 'react-icons/fa'
@@ -19,32 +20,46 @@ const about = () => {
           <div className='about_me-image'>
             <img src={ME} alt='photo of Jeffrey' />
           </div>
+          {/* <div className='about_me-image'>
+            <img src={US} alt='Jeffrey and family' />
+          </div> */}
        </div>
+       {/* about me header cards */}
        <div className="about_content">
         <div className="about_cards">
           <article className='about_card'>
             <RiHeartsFill className='about_icon'/>
             <h5>Family</h5>
-            <small>Happy husband and father</small>
+            <small>Happy Husband and Father</small>
           </article>
           <article className='about_card'>
             <FaDiceD20 className='about_icon'/>
             <h5>Hobbies</h5>
-            <small>D+D, Board Games, Painting</small>
+            <ul>
+            <li><small>D&D</small></li>
+            <li><small>Coding</small></li>
+            <li><small>Painting</small></li> 
+            </ul>
+            
           </article>
           <article className='about_card'>
             <GiBookCover className='about_icon'/>
-            <h5>Currently Reading</h5>
-            <small>The Priory of the Orange Tree</small>
+            <h5>Interests</h5>
+            <ul>
+            <li><small>Board Games</small></li>
+            <li><small>Reading</small></li>
+            <li><small>Cooking</small></li> 
+            </ul>
           </article>
         </div>
 
         <p>
-           I'm Jeffrey, a full-stack developer with a passion for delievering <span>⭐⭐⭐⭐⭐</span> designs that are elegant and intuitive.  While the majority of my experience has been developing brand and e-commerce sites, I have also worked on a broad spectrum project types.  I'm no slouch when it comes to backend development either, as I've created multiple full-stack projects and have experience with business intelligence data management. 
+           I'm Jeffrey, a full-stack developer with a passion for delievering <span>⭐⭐⭐⭐⭐</span> designs that are elegant and intuitive.  While the majority of my experience has been developing brand and e-commerce sites, I have also worked on a broad spectrum project types.  I'm no novice when it comes to backend development either.  I've created multiple full-stack projects and have experience with BI data management. 
         </p>
         <p>
           As a developer, I'm most driven by taking a clients' idea and making it a reality.  I don't adhere to any one style, because YOUR website should be a true representation of YOU or your brand's style and vibe.  I hope you will share your vision and allow me to help shape your digital world.
         </p>
+        {/* <p>When I'm not not clacking away at my keyboard, I try to spend as much time as I can with my wife Erin and our daughter Zelda.  We have a little land that they both are trying their best to fill with as many animals as possible.  I enjoy playing board games, Dungeons and Dragons, reading and cooking.</p> */}
         
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
        </div>
