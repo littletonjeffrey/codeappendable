515b00ec1aadb6e4e009dbef6ff1fbf8d78b243e
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-30 17:06:14 -0500

    contact section and styling done

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 //connect individual style sheet
 import './contact.css'
+//icons for contacting
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
 
 const contact = () => {
   return (
     <section id='contact'>
-     Contact
+     <h5>Get In Touch</h5>
+     <h2>Contact Me</h2>
+
+     <div className="container contact_container">
+       <div className="contact_options">
+         <article className="contact_option">
+          <MdOutlineEmail className='contact_option-icon'/> 
+           <h4>Email</h4>
+           <h5>littletonjeffrey@gmail.com</h5>
+           <a href="mailto:littletonjeffrey@gmail.com" target="_blank">Send a message</a>
+         </article>
+         <article className="contact_option">
+          <RiMessengerLine className='contact_option-icon'/> 
+           <h4>Messenger</h4>
+           <h5>@jeffrey.littleton.5</h5>
+           <a href="https://m.me/jeffrey.littleton.5" target="_blank">Send a message</a>
+         </article>
+         <article className="contact_option">
+         <RiMessengerLine className='contact_option-icon'/>
+           <h4>WhatsApp</h4>
+           <h5>+123456789</h5>
+           <a href="https://api.whatsapp.com/send?phone+5127911783" target="_blank">Send a message</a>
+         </article>
+       </div>
+       {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+     </div>
     </section>
   )
 }
--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -5,43 +5,57 @@ import './testimonials.css'
 import JR from '../../assets/jaden.png'
 import WP from '../../assets/william.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 const testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Word of Mouth</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
-        <article className="testimonial">
+      <Swiper className="container testimonials_container"
+       // install Swiper modules
+       modules={[ Pagination ]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}>
+        <SwiperSlide className="testimonial">
           <div className="avatar">
-            <img src ="https://ca.slack-edge.com/T03ENUM2YP6-U03KJS6TT40-a81aac4951bc-512" />
+            <a href='https://missatrox44.github.io/portfolio' target='blank'><img src ="https://ca.slack-edge.com/T03ENUM2YP6-U03KJS6TT40-a81aac4951bc-512" /></a>
           </div>
-          <a href='https://missatrox44.github.io/portfolio'><h5>Sara Baqla</h5></a>
+          <a href='https://missatrox44.github.io/portfolio' target='blank'><h5>Sara Baqla</h5></a>
           <small className="review">"Jeffrey has a great eye for UI/UX design and thrives in front end development. His knowledge as a full-stack developer is exceptional and he is an incredible asset to any team. I look forward to our future collaborations!"</small>
-          
-        </article>
-        <article className="testimonial">
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="avatar">
-            <img src ={JR} />
+            <a href='https://www.jadent.dev/' target='blank'><img src ={JR} /></a>
           </div> 
-          <a href='https://www.jadent.dev/'><h5>Jaden Rodriguez</h5></a>
-          <small className="review">"Jeffrey has a great eye for UI/UX design and thrives in front end development. His knowledge as a full-stack developer is exceptional and he is an incredible asset to any team. I look forward to our future collaborations!"</small>
-        </article>
-        <article className="testimonial">
+          <a href='https://www.jadent.dev/' target='blank'><h5>Jaden Rodriguez</h5></a>
+          <small className="review">"Jeffrey is an amazing developer, who is always finding solutions to his problems, while eager to help you with yours. Its honestly amazing to see how fast he went from learning how to create an image tag, to being able to code a full stack application."</small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="avatar">
             <img src ={WP}/>
           </div>
           <h5>William Pollock</h5>
           <small className="review">"Jeffrey is a natural people leader.  He has a tremendous drive for achievement that is only rivaled by his wanting to help others succeed."</small>
-        </article>
-        <article className="testimonial">
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
           <div className="avatar">
-            <img src ="https://ca.slack-edge.com/T03ENUM2YP6-U03KJS6TT40-a81aac4951bc-512" />
+            <a href='https://missatrox44.github.io/portfolio' target='blank'><img src ="https://ca.slack-edge.com/T03ENUM2YP6-U03KJS6TT40-a81aac4951bc-512" /></a>
           </div>
-          <h5>Sara Baqla</h5>
+          <a href='https://missatrox44.github.io/portfolio' target='blank'><h5>Sara Baqla</h5></a>
           <small className="review">Jeffrey has a great eye for UI/UX design and thrives in front end development. His knowledge as a full-stack developer is exceptional and he is an incredible asset to any team. I look forward to our future collaborations!</small>
-        </article>
-      </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   )
 }
