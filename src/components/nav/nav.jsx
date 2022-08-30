d9e455250f92a1476eb9900db5441f0da65bbc0e
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-29 19:09:06 -0500

    resized header icons, added nav icons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 //connect individual style sheet
 import './nav.css'
+// importing icons for nav bar
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {RiSuitcaseLine} from 'react-icons/ri'
+import {HiOutlineDesktopComputer} from 'react-icons/hi'
+import {FiMessageSquare} from 'react-icons/fi'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><RiSuitcaseLine /></a>
+      <a href='#services'><HiOutlineDesktopComputer /></a>
+      <a href='#contact'><FiMessageSquare /></a>
+    </nav>
   )
 }
 
